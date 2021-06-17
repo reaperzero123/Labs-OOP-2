53ca6c70e30e78aa36830bba4988cde0ad7205bc
ReapeZer <46023866+reaperzero123@users.noreply.github.com>
2021-06-17 20:08:17 +0300

    Add files via upload

---
+++ lab13/13лаба(MVVM)/Model/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace _13лаба_MVVM_.Model
+{
+    class DB
+    {
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string DayOfWeak { get; set; }
+        public DateTime Date { get; set; }
+        public Typess Type { get; set; }
+        public int Seats { get; set; }
+        public int Price { get; set; }
+    }
+
+    enum Typess
+    {
+        Отработка, Консультация, Доп
+    }
+
+    class CustomerContext : DbContext
+    {
+        public CustomerContext() : base("DBContext") { }
+        public DbSet<DB> Concert { get; set; }
+    }
+}
